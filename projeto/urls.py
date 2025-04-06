c4c9be9ff288cd40999b1a76f4da6d2de56fd488
luizherbet <luizherbetsouza@gmail.com>
2025-04-06 18:59:22 -0300

    layout base

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def my_view(request):
-    return HttpResponse("Fábrica do poema")
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sobre/", my_view)
+    path('',include('recipes.url'))
+#    path('recipes/',include('recipes.url'))
 ]
