fac9d0558a7318847f8c2cd26f856a00180636e6
luizherbet <luizherbetsouza@gmail.com>
2025-04-07 18:54:40 -0300

    recipe

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name' : 'Zama',})
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html', context={'name' : 'Recipe',})
